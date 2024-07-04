8079377da4f3b0f13c728c4e9da21822bd1f8c76
riot <karachristos@outlook.com>
2024-07-04 15:26:06 +0300

    refactor: export functionality to remove duplications
    WIP: resizing an image

--- src/exportImage.ts
+++ src/exportImage.ts
@@ -1,40 +1,44 @@
-import { createCanvas, createImageData, type ImageData } from 'canvas';
+import { createCanvas, createImageData } from 'canvas';
 import fs from 'fs';
+
+import type { ImageData } from 'canvas';
 
 function createCanvasData(
   input: Uint8ClampedArray,
   width: number,
   height: number,
-): Buffer {
+  newWidth = width,
+  newHeight = height
+) {
   const canvas = createCanvas(width, height);
   const ctx = canvas.getContext('2d');
 
-  const inputAsImageData = createImageData(input, input.length / (4 * height), input.length / (4 * width));
+  const inputAsImageData = createImageData(
+    input,
+    width,
+    height,
+  );
 
-  let pixelArrayToStore: ImageData = inputAsImageData;
+  ctx.putImageData(inputAsImageData, 0, 0);
 
-  if (inputAsImageData.height !== height || inputAsImageData.width !== width) {
-    pixelArrayToStore = resizeImage(inputAsImageData, width, height);
-  }
-
-  ctx.putImageData(pixelArrayToStore, 0, 0);
-
-  const imageBase64String = canvas
-    .toDataURL()
-    .replace(/^data:image\/\w+;base64,/, '');
-
-  return Buffer.from(imageBase64String, 'base64');
+  return newHeight !== height || newWidth !== width
+    ? resizeImage(inputAsImageData, newWidth, newHeight)
+    : canvas;
 }
 
 export function exportAsJpeg(
   imageData: Uint8ClampedArray,
   width: number,
   height: number,
+  newWidth = width,
+  newHeight = height
 ): boolean {
-  const canvasData = createCanvasData(imageData, width, height);
+  const canvasData = createCanvasData(imageData, width, height, newWidth, newHeight);
+
+  const jpegBuffer = canvasData.toBuffer('image/jpeg', { quality: 1 });
 
   try {
-    fs.writeFileSync('./image.jpeg', canvasData);
+    fs.writeFileSync('./image.jpeg', jpegBuffer);
     return true;
   } catch (error) {
     console.error('Error writing image to file', error);
@@ -46,11 +50,15 @@ export function exportAsPng(
   imageData: Uint8ClampedArray,
   width: number,
   height: number,
+  newWidth = width,
+  newHeight = height
 ): boolean {
-  const canvasData = createCanvasData(imageData, width, height);
+  const canvasData = createCanvasData(imageData, width, height, newWidth, newHeight);
+
+  const pngBuffer = canvasData.toBuffer('image/png');
 
   try {
-    fs.writeFileSync('./image.png', canvasData);
+    fs.writeFileSync('./image.png', pngBuffer);
     return true;
   } catch (error) {
     console.error('Error writing image to file', error);
@@ -58,13 +66,56 @@ export function exportAsPng(
   }
 }
 
-function resizeImage(
-  imageData: ImageData,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  width: number,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  height: number,
-): ImageData {
-  // resize the image
-  return imageData;
+// NOT USED YET
+// WIP
+function resizeImage(image: ImageData, width: number, height: number) {
+  console.log('asde')
+  const canvas = createCanvas(image.width, image.height);
+  const ctx = canvas.getContext('2d');
+
+  const oldAspectRatio = image.width / image.height;
+
+  const widthResizeFactor = width / image.width;
+  const heightResizeFactor = height / image.height;
+
+  const newAspectRatio = widthResizeFactor / heightResizeFactor;
+
+  let newPixelsArray: Uint8ClampedArray;
+
+  if (oldAspectRatio !== newAspectRatio) {
+    throw new Error('Cannot resize image without keeping aspect ratio');
+  } else {
+    newPixelsArray = new Uint8ClampedArray(image.width * image.height * 4 * newAspectRatio);
+
+    const startOfLine = 4 * width;
+
+    console.log('input', {
+      widthResizeFactor,
+      heightResizeFactor,
+      width,
+      height
+    }, '\n')
+
+    for (let i = 0; i < image.data.length; i += 4) {
+      const pixel = image.data.slice(i, i + 4);
+
+      const oldPixelPosition = Math.floor(i / 4);
+      
+      for (let x = 0; x < widthResizeFactor; x++) {
+        const pixelPositionInXAxis = oldPixelPosition * 4 + x * 4;
+        const mirroredPixelPositionInXAxis = (width * 4) - oldPixelPosition * 4 - (x + 1) * 4;
+        for (let y = 0; y < heightResizeFactor; y++) {
+          const currentLine = startOfLine * y;
+          console.log('Setting pixel', { oldPixelNumber: oldPixelPosition, currentBlockStart: startOfLine * y, currentBlockEnd: startOfLine * (y + 1), currentLine, pixelPositionInXAxis, mirroredPixelPositionInXAxis});
+          newPixelsArray.set(pixel, currentLine + pixelPositionInXAxis + x * 4);
+          newPixelsArray.set(pixel, currentLine + mirroredPixelPositionInXAxis - x * 4);
+        }
+      }
+      if ( i === 1 ) throw new Error('break')
+    }
+  } 
+
+  ctx.putImageData(createImageData(newPixelsArray, width, height), 0, 0);
+
+  return canvas;
 }
