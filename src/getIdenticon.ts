605a9c5966872cdf8c81baa9375cebb533d4cbe3
riot <karachristos@outlook.com>
2024-07-16 18:40:35 +0300

    fix: lodash concat and reverse dont work together

--- src/getIdenticon.ts
+++ src/getIdenticon.ts
@@ -14,11 +14,11 @@ function getColor(input: number[]): TColor {
 export function buildIdenticonArray(input: number[], width: number, height: number) {
   const limitedArray = _.take(input, Math.ceil((width * height) / 2));
   const binaryRepresentation = _.map(limitedArray, value => (value % 2 === 0 ? 1 : 0));
-  const lineChunks = _.chunk(binaryRepresentation, Math.ceil(width / 2));
-  const identiconArray = _.flatMap(lineChunks, value => _.concat(value, _.reverse(value)));
+  const lineChunks = _.chunk(binaryRepresentation, width / 2);
+  const identiconArray = _.flatMap(lineChunks, array => [...array, ...array.reverse()]);
 
   return {
-    color: getColor(input),
+    color: getColor(limitedArray),
     data: identiconArray
   };
 }
